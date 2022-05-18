bfc4b1ce026bdf0dcdc41e6bc50ee13af51616bb
arifraj199 <karif9514@gmail.com>
2022-05-19 05:08:14 +0600

    post api implement

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
@@ -7,6 +8,27 @@ require('dotenv').config();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n6reu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const todoCollection = client.db("todo_task").collection("tasks");
+
+        app.post("/addtask",async(req,res)=>{
+            const task = req.body;
+            const result = await todoCollection.insertOne(task);
+            res.send(result);
+
+        })
+    }
+    finally{}
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
